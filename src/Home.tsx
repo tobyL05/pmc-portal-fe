82aa15a9f2882d5b4c1151f4f90d3de563d7411a
Toby <toby.lau075@gmail.com>
2024-07-05 14:44:05 -0700

    created basic login button

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,24 +1,53 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import { GoogleAuthProvider, getRedirectResult, signInWithPopup, signInWithRedirect } from 'firebase/auth';
+import { GoogleAuthProvider, inMemoryPersistence, setPersistence, signInWithPopup } from 'firebase/auth';
 import { auth } from "../firebase"
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  const authProvider = new GoogleAuthProvider()
 
-  useEffect(() => {
-    async () => {
-      const response = await getRedirectResult(auth)
-      if (response) {
-        console.log(response.user)
+  // session is saved to cookie
+
+  const navigateTo = useNavigate()
+
+  async function checkAuth() {
+      const checkAuth = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/test`, {
+        credentials: "include"
+      })
+      return checkAuth.ok
+  }
+
+  async function login() {
+    setPersistence(auth,inMemoryPersistence)
+    const authProvider = new GoogleAuthProvider()
+    let signInResult;
+    try {
+        signInResult = await signInWithPopup(auth, authProvider)
+        const user = signInResult.user;
+        const idToken = await user.getIdToken()
+        const req = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/login`, {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            user: user.toJSON(),
+            idToken: idToken
+          })
+        })
+        if (req.ok && await checkAuth()) {
+          console.log("authenticated")
+          // navigateTo("/dashboard")
+        }
+      } catch (error) {
+        console.log(error);
       }
-    }
-  })
+  }
 
   return (
     <>
       <h1>Welcome to PMC</h1>
-      <button onClick={() =>{signInWithRedirect(auth, authProvider)}}>
+      <button onClick={login}>
         login with Google
       </button>
     </>

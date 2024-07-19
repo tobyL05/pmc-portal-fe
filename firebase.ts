9e95d14bf1e55d3178b4cbc6b2029119357fd371
Toby <toby.lau075@gmail.com>
2024-07-19 10:14:03 +0700

    change firebase-config path

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./.secret/firebase-config"
+import firebaseConfig from "./.secret/firebase-config.json"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries

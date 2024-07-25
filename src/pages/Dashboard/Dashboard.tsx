b69576053590d55d2fdd3387f39c7140272ad343
Toby <toby.lau075@gmail.com>
2024-07-25 15:29:58 +0700

    fixed routing

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+export default function Dashboard() {
+    // Check if logged in or continued as non-member
+    
+    return (
+        <div>
+            <h1>Dashboard goes here</h1>
+        </div>
+    )
+}

e7887ac475a058d33e59b1f75ea1eaeb8a68cffe
karenag <karenag@student.ubc.ca>
2024-08-04 02:38:34 -0700

    added a new event page, having trouble fetching the event details from backend API

--- src/components/OnboardingForm/OnboardingForm.tsx
+++ src/components/OnboardingForm/OnboardingForm.tsx
@@ -11,50 +11,50 @@ import PMCLogo from "../../assets/pmclogo.svg"
 
 
 const UserZodObj = z.object({
-    first_name: z.string().min(1,{
+    first_name: z.string().min(1, {
         message: "Please enter a first name."
     }),
 
-    last_name: z.string().min(1,{
+    last_name: z.string().min(1, {
         message: "Please enter a last name."
     }),
 
-    ubc_student: z.enum(["yes","no, other uni","no, other"],{
+    ubc_student: z.enum(["yes", "no, other uni", "no, other"], {
         message: "Please select a value."
     }),
     student_id: z.number({
         coerce: true
     })
-    .int({
-        message: "Student IDs must not have decimal points!"
-    })
-    .gte(10000000, {
-        message: "Please enter a valid 8-digit student ID."
-    })
-    .lte(99999999, {
-        message: "Please enter a valid 8-digit student ID."
-    })
-    .optional(),
+        .int({
+            message: "Student IDs must not have decimal points!"
+        })
+        .gte(10000000, {
+            message: "Please enter a valid 8-digit student ID."
+        })
+        .lte(99999999, {
+            message: "Please enter a valid 8-digit student ID."
+        })
+        .optional(),
 
-    year: z.enum(["1","2","3","4","4+"], {
+    year: z.enum(["1", "2", "3", "4", "4+"], {
         message: "Please select a value."
     }).optional(),
 
-    faculty: z.string().min(1,{
+    faculty: z.string().min(1, {
         message: "Please enter a valid faculty."
     }).optional(),
 
-    major: z.string().min(1,{
+    major: z.string().min(1, {
         message: "Please enter a valid major."
     }).optional(),
 
-    why_pm: z.string().min(1,{
+    why_pm: z.string().min(1, {
         message: "Why would you like to join PMC?"
     }).max(300, {
         message: "Maximum 300 characters."
     }),
 
-    returning_member: z.enum(["yes","no"],{
+    returning_member: z.enum(["yes", "no"], {
         message: "Please select a value."
     })
 })
@@ -79,9 +79,9 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
         handleSubmit,
         watch,
         formState: { errors },
-      } = useForm<UserSchema>({
+    } = useForm<UserSchema>({
         resolver: zodResolver(UserZodObj)
-      })
+    })
 
     const student_status = watch("ubc_student")
 
@@ -96,15 +96,15 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
             unregister("faculty")
             unregister("major")
         }
-    },[student_status])
+    }, [student_status])
 
     const onSubmit = async (data: UserSchema) => {
         // fetch onboarding endpoint
-        const onboarding = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/onboarding`,{
+        const onboarding = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/onboarding`, {
             method: "POST",
             credentials: "include",
             headers: {
-              'Content-type': 'application/json',
+                'Content-type': 'application/json',
             },
             body: JSON.stringify({
                 creds: creds,
@@ -112,11 +112,11 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
                     displayName: user.displayName,
                     email: user.email,
                     pfp: user.photoURL,
-                    ... data
+                    ...data
                 }
             })
         })
-        
+
         if (onboarding.ok) {
             navigateTo("/dashboard")
         } else {
@@ -132,7 +132,7 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
     return (
         <div className="onboarding-container">
             <div className="onboarding-content">
-                <img className="onboarding-content--logo" src={PMCLogo}/>
+                <img className="onboarding-content--logo" src={PMCLogo} />
                 <h1 className="onboarding-content-header pmc-gradient-text">Create your account</h1>
                 <form autoComplete="off" className="onboarding-form" onSubmit={handleSubmit(onSubmit)}>
                     <div className="onboarding-form-content">
@@ -154,7 +154,7 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
                         </div>
 
                         <div className="onboarding-form-select--container">
-                            <select required className="select-ubcstudent" {...register("ubc_student",{required: "please select a value"})}>
+                            <select required className="select-ubcstudent" {...register("ubc_student", { required: "please select a value" })}>
                                 <option value="" hidden>Are you a UBC student?</option>
                                 <option value={"yes"}>Yes, I'm a UBC student.</option>
                                 <option value={"no, other uni"}>No, I'm from another university.</option>
@@ -163,7 +163,7 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
                             {errors.ubc_student && <span>{errors.ubc_student.message}</span>}
                         </div>
 
-                        {student_status === "yes" && 
+                        {student_status === "yes" &&
                             <div className="onboarding-form-content--row">
                                 <FormInput
                                     type="text"
@@ -176,10 +176,10 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
 
                         }
 
-                        {student_status !== "no, other" && 
+                        {student_status !== "no, other" &&
                             <div className="onboarding-form-content--row">
                                 <div className="onboarding-form-select--container">
-                                    <select style={{"width": "7rem"}} required {...register("year",{required: "please select a value"})}>
+                                    <select style={{ "width": "7rem" }} required {...register("year", { required: "please select a value" })}>
                                         <option value="" hidden>Year</option>
                                         <option value={"1"}>1</option>
                                         <option value={"2"}>2</option>
@@ -205,9 +205,9 @@ export default function OnboardingForm({ user, creds }: { user: User, creds: log
                                     error={errors.major}
                                 />
                             </div>
-                        }       
+                        }
                         <div className="onboarding-form-select--container">
-                            <select required {...register("returning_member",{required: "Please select a value."})}>
+                            <select required {...register("returning_member", { required: "Please select a value." })}>
                                 <option value="" hidden>Are you a returning member?</option>
                                 <option value="yes">Yes, I'm a returning PMC member.</option>
                                 <option value="no">No, I'm new to PMC.</option>
